8c3ad82c2ffbfe695a4ad676fa3b7a19aa28620d
Marshe3 <rkddus97@gmail.com>
2026-04-20 14:35:40 +0900

    feat: Cout Cin 모두 사용해 보

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -7,5 +7,16 @@ int main() {
 
 	cout << "Hello, World!2" << endl;
 
+	char userName[50];
+
+	cout << "[ User Name ]\n";
+	cout << "input your name : ";
+
+	cin >> userName;
+
+	cout << "::::::::::Welcome to the D&D World ::::::::::\n";
+	cout << "User Name : [" << userName << "]!\n";
+	
+
 	return 0;
 }
